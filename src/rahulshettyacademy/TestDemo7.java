cee90f8761f82f0d5fcc7d82ed8e7e8a734fb270
Prashant7400 <prashanttaware967400@gmail.com>
2023-09-18 10:46:19 +0530

    y person add new test on 18Sep23

---
+++ src/rahulshettyacademy/TestDemo7.java
@@ -0,0 +1,14 @@
+ package rahulshettyacademy;
+
+import org.testng.annotations.Test;
+
+
+public class TestDemo7{
+
+	@Test
+public void developbranch() {
+	
+
+	System.out.println(" 18 Sep 2023 added on master Branch");
+}
+}
